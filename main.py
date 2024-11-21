c526ddc20252a3971311e7e51605a17e958be43b
Windyy-1007 <namphongtip02@gmail.com>
2024-11-21 22:27:39 +0700

    make changes, genetic algorithm

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import gym_cutting_stock
 import gymnasium as gym
 from policy import GreedyPolicy, RandomPolicy
-from student_submissions.s2210xxx.policy2210xxx import Policy2210xxx
+from student_submissions.s2210xxx.policy2210xxx import Policy2352906
 
 # Create the environment
 env = gym.make(
@@ -11,48 +11,53 @@ env = gym.make(
 NUM_EPISODES = 100
 
 if __name__ == "__main__":
-    # Reset the environment
-    observation, info = env.reset(seed=42)
-
-    # Test GreedyPolicy
-    gd_policy = GreedyPolicy()
-    ep = 0
-    while ep < NUM_EPISODES:
-        action = gd_policy.get_action(observation, info)
-        observation, reward, terminated, truncated, info = env.step(action)
-
-        if terminated or truncated:
-            observation, info = env.reset(seed=ep)
-            print(info)
-            ep += 1
-
-    # Reset the environment
-    observation, info = env.reset(seed=42)
-
-    # Test RandomPolicy
-    rd_policy = RandomPolicy()
-    ep = 0
-    while ep < NUM_EPISODES:
-        action = rd_policy.get_action(observation, info)
-        observation, reward, terminated, truncated, info = env.step(action)
-
-        if terminated or truncated:
-            observation, info = env.reset(seed=ep)
-            print(info)
-            ep += 1
-
-    # Uncomment the following code to test your policy
     # # Reset the environment
     # observation, info = env.reset(seed=42)
-    # print(info)
+    
+    # # print(info)
+    # print(observation)
 
-    # policy2210xxx = Policy2210xxx()
-    # for _ in range(200):
-    #     action = policy2210xxx.get_action(observation, info)
+    # # Test GreedyPolicy
+    # gd_policy = GreedyPolicy()
+    # ep = 0
+    # while ep < NUM_EPISODES:
+    #     action = gd_policy.get_action(observation, info)
     #     observation, reward, terminated, truncated, info = env.step(action)
-    #     print(info)
 
     #     if terminated or truncated:
-    #         observation, info = env.reset()
+    #         observation, info = env.reset(seed=ep)
+    #         print(info)
+    #         ep += 1
+
+    # # Reset the environment
+    # observation, info = env.reset(seed=42)
+
+    # # Test RandomPolicy
+    # rd_policy = RandomPolicy()
+    # ep = 0
+    # while ep < NUM_EPISODES:
+    #     action = rd_policy.get_action(observation, info)
+    #     observation, reward, terminated, truncated, info = env.step(action)
+
+    #     if terminated or truncated:
+    #         observation, info = env.reset(seed=ep)
+    #         print(info)
+    #         ep += 1
+
+    # Uncomment the following code to test your policy
+    # Reset the environment
+    observation, info = env.reset(seed=42)
+    print(info)
+
+    policy2210xxx = Policy2352906()
+    ep = 0
+    for _ in range(200):
+        action = policy2210xxx.get_action(observation, info)
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        if terminated or truncated:
+            observation, info = env.reset()
+            print(info)
+            ep += 1
 
 env.close()
--- student_submissions/s2210xxx/policy2210xxx.py
+++ student_submissions/s2210xxx/policy2210xxx.py
@@ -1,14 +1,69 @@
 from policy import Policy
+import heapq
+import numpy as np  # Add this import
 
-
-def Policy2210xxx(Policy):
+class Policy2352906(Policy):
     def __init__(self):
-        # Student code here
-        pass
+        self.best_solution = None
+        self.best_value = float('-inf')
+        
+    def get_fitness(self, observation, info):
+        list_prods = observation["products"]
+        filled_ratio = info["filled_ratio"]
+        total_area = filled_ratio * sum(prod['size'][0] * prod['size'][1] for prod in list_prods)
+        occupied_area = 0
 
+        for prod in list_prods:
+            if prod["quantity"] > 0:
+                occupied_area += prod["size"][0] * prod["size"][1] * prod["quantity"]
+
+        wasted_area = total_area - occupied_area
+        if wasted_area <= 0:
+            return float('inf') 
+        return 1 / wasted_area
+    
+    def get_best_solution(self, observation, info):
+        list_prods = observation["products"]
+        prod_size = [0, 0]
+        stock_idx = -1
+        pos_x, pos_y = 0, 0
+        stock = None
+        for prod in list_prods:
+            if prod["quantity"] > 0:
+                prod_size = prod["size"]
+                for i, s in enumerate(observation["stocks"]):
+                    stock = s
+                    stock_w, stock_h = self._get_stock_size_(stock)
+                    prod_w, prod_h = prod_size
+                    if stock_w < prod_w or stock_h < prod_h:
+                        continue
+                    for x in range(stock_w - prod_w + 1):
+                        for y in range(stock_h - prod_h + 1):
+                            if self._can_place_(stock, (x, y), prod_size):
+                                stock_idx = i
+                                pos_x, pos_y = x, y
+                                break
+                        if stock_idx != -1:
+                            break
+                    if stock_idx != -1:
+                        break
+                if stock_idx != -1:
+                    break
+        return stock_idx, pos_x, pos_y
+    
     def get_action(self, observation, info):
-        # Student code here
-        pass
+        list_prods = observation["products"]
 
-    # Student code here
-    # You can add more functions if needed
+        prod_size = [0, 0]
+        stock_idx = -1
+        pos_x, pos_y = 0, 0
+        
+        stock_idx, pos_x, pos_y = self.get_best_solution(observation, info)
+        if stock_idx == -1:
+            return {"stock_idx": -1, "size": [0, 0], "position": (0, 0)}
+        return {"stock_idx": stock_idx, "size": prod_size, "position": (pos_x, pos_y)}
+    
+    
+    
+    
+        
